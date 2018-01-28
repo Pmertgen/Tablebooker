7f1681cd810ce632b0629510994b060a7abf7324
phil <phil@home.de>
2018-01-28 20:40:20 +0100

    Some improvements on the logic For example an else condition in the main.php was superfluous

--- choosetable.php
+++ choosetable.php
@@ -1,16 +1,7 @@
 <?php
 require "header.html";
 
-?>
 
-<div class="container">
-<div class="card">
-  <div class="card-header bg-warning">
-    Tablebooker 1.0
-  </div>
-  <div class="card-body bg-secondary">
-
-<?php
 if (isset($_POST['reserve'])) {
 	//$time = $_POST["time"];
 	//$date = $_POST["date"];
@@ -19,11 +10,7 @@ if (isset($_POST['reserve'])) {
 	echo "You chose table number: " . $table . "</br>";
 	//echo "This is for: " . $time . $date; 
 
-} else{
-
-	echo "Please select a table to reserve</br>";
-
-}
+} 
 ?>
 </div>
 </div>
