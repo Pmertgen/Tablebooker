49b4b59479eed99c5799c1dca00f73e0fe25a519
phil <phil@home.de>
2018-01-29 20:51:53 +0100

    added the function that echoes out the date and time

--- index.php
+++ index.php
@@ -16,8 +16,12 @@ error_reporting(0);
 if (isset($_POST["submitguests"]))
 {
   $people = $_POST["people"];
+  $time = $_POST["time"];
+  $date = $_POST["date"];
 
     echo "<h2>You chose to book for " . $people . " people.</h2></br>";
+    echo "the time: " . $time . "</br>";
+    echo "date: " . $date . "</br>";
 }
 
 require_once("dbconn.php");
